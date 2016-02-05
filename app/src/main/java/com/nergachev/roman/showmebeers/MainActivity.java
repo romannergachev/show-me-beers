45a0938db15ad998f57eca45d8c3f41e8dc88f45
Roman Nergachev <nergachev.roman@gmail.com>
2016-02-05 23:09:40 +0100

    init commit

---
+++ app/src/main/java/com/nergachev/roman/showmebeers/MainActivity.java
@@ -0,0 +1,38 @@
+package com.nergachev.roman.showmebeers;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.beer_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        int i = 0;
+        String[] myDataset = new String[150];
+        for (int j = 0;j<myDataset.length;j++){
+            myDataset[j] = "abc  " + i;
+            i++;
+        }
+        mAdapter = new BeerAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
